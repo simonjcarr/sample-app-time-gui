5e35fbdfdcd0c8ae6c53a38533c5436e3d73b144
simonjcarr <simonjcarr@gmail.com>
2022-04-16 18:54:50 +0100

    added keycloak integration

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Keycloak from 'keycloak-js'
 
-createApp(App).mount('#app')
+const initOptions = {
+  url: 'https://keycloak.apps.soxprox.local', realm: 'soxprox', clientId: 'time-app', onLoad: 'login-required'
+}
+
+let keycloak = Keycloak(initOptions)
+
+keycloak.init({
+  onLoad: initOptions.onLoad
+}).then((auth) => {
+  if(!auth) {
+    window.location.reload()
+  } else {
+    createApp(App).mount('#app')
+  }
+  setInterval(() => {
+    keycloak.updateToken(70)
+  }, 6000)
+})
+
+
